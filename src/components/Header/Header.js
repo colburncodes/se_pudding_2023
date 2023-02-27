a4257af01af45b51b192c37efcc3190df9dc0400
colburncodes <colburnsanders@gmail.com>
2023-02-27 16:44:02 -0600

    draft layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/love_logo.jpg";
+import "./Header.css";
 
 export function Header() {
-  return <header className="App-header">Header Component</header>;
+  return (
+    <header className="header">
+      <div className="header__container">
+        <Link to={"/"}>
+          <img className="header__logo" src={logo} alt="Serendipity Logo" />
+        </Link>
+        <h1 className="header__title">Header Component</h1>
+      </div>
+    </header>
+  );
 }
