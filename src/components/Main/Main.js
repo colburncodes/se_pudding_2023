bdfdcf6bf2f24e3ca722b40cf640483ad5d698b8
khuephamy <khuephumy19@gmail.com>
2023-02-28 19:23:50 -0600

    fix conflict

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Introduction from "./Introduction/Introduction";
+import Introduction from "./Introduction/Introduction.js";
 export function Main() {
   return (
     <>
