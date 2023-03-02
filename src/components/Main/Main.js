450d45f13b1bc230dbefbb2b4ed5285b1171f0a5
colburncodes <colburnsanders@gmail.com>
2023-03-01 20:37:52 -0600

    update

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Introduction from "./Introduction/Introduction.js";
+import { Chart } from "../Chart/Chart";
+
 export function Main() {
   return (
     <>
       <Introduction />
       <main className="main">
         <div className="main__container">
-          <section className="main__graph-one">Section 1</section>
+          <section className="main__graph-one">
+            <Chart />
+          </section>
           <section className="main__graph-two">Section 2</section>
           <section className="main__graph-three">Section 3</section>
         </div>
