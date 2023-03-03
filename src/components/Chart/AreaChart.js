a5a8fa6d8423bc3f67d94fe948c4355303621864
colburncodes <colburnsanders@gmail.com>
2023-03-03 17:26:15 -0600

    update

---
+++ src/components/Chart/AreaChart.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export function AreaChart() {
+  return <div></div>;
+}
