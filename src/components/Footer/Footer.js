2c420ad086fd3aae93027b594f72d998c98f2432
colburncodes <colburnsanders@gmail.com>
2023-03-04 07:01:36 -0600

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./Footer.css";
 
 export function Footer() {
-  return <footer className="footer" id="footer"> 
-  © 2022 This Visual Essay is made by Colburn, Devin, Chani and Khue
-  </footer>;
+  return (
+    <footer className="footer" id="footer">
+      © 2022 This Visual Essay is made by Colburn, Josue, Chani and Khue
+    </footer>
+  );
 }
